6628c16a31c19f1bc3cb1a89298036b75c076d05
Erdem Elvan <erdemelvan35@gmail.com>
2021-11-29 15:06:39 +0300

    html,cssi js structure and mobile nav added

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict';
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav--visible');
+})
